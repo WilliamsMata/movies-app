3549b06e8d6f1da0eb8b1cd3b4732fbd9dc20442
williamsmata <williams.rm99@gmail.com>
2024-03-06 09:33:08 -0400

    Add navigation and home and details screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import {Text, View} from 'react-native';
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './presentation/navigation/Navigation';
 
 export default function App() {
   return (
-    <View>
-      <Text>Movies App</Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
