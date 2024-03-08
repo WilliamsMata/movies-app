fec30b0fc427d2c70f602447995087e73252682f
williamsmata <williams.rm99@gmail.com>
2024-03-08 15:11:48 -0400

    Updating environment variables and dependencies configuration

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '609bd26399a7cef0677a9669b3c35309',
+    api_key: THE_MOVIE_DB_KEY,
     // language: 'es',
   },
 });
