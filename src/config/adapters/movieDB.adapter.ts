c1c8b87b6633a0f3ffe2923352efbb33b47532ad
williamsmata <williams.rm99@gmail.com>
2024-03-08 12:30:25 -0400

    Update MovieDB API language to English

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -4,6 +4,6 @@ export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
     api_key: '609bd26399a7cef0677a9669b3c35309',
-    language: 'es',
+    // language: 'es',
   },
 });
